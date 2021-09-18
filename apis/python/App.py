c689519b55374abe5164bb92b4fd5e501da19447
Feliciano07 <fernandoxhajon122@gmail.com>
2021-09-18 15:59:33 -0600

    cambios para mssql python

--- apis/python/App.py
+++ apis/python/App.py
@@ -1,19 +1,32 @@
 from flask import Flask,jsonify,request
-from flaskext.mysql import MySQL
 from flask_cors import CORS
+import pymssql
 
 app = Flask(__name__)
 
-#configuracion credenciales de la primera base de datos
-mysql = MySQL(app, prefix="mysql1", host="localhost", user="root",
-              password="proyectosusac", db="proyecto1", autocommit=True)
-
-mysql_1 = MySQL(app, prefix="mysql2", host="localhost", user="root",
-              password="proyectosusac", db="respaldo", autocommit=True)
 
 
+#funciones para obtener establecer las conexiones
+def obtener_conexion1():
+    conexion1 = pymssql.connect(
+        host ='sopes-1.database.windows.net',
+        user = 'sopes-1',
+        password = 'DqDDyZl8PmK9n6Zj',
+        database = 'proyecto-1',
+    )
+    return conexion1
 
-#configuracion de cors, todos los origines
+def obtener_conexion2():
+    conexion2 = pymssql.connect(
+        host ='sopes-1.database.windows.net',
+        user = 'sopes-1',
+        password = 'DqDDyZl8PmK9n6Zj',
+        database = 'proyecto-1',
+    )
+    return conexion2
+
+
+#configuracion de cors, todos los origenes
 CORS(app)
 
 @app.route('/')
@@ -40,57 +53,54 @@ def finalizarCarga():
 def publicacion():
     datos = Convert(request.json)
     # se realizan las operaciones de insertar en la primera base de datos
-    salida_hashtags = ingresar_hashtags(datos["hashtags"])
     salida_publicacion = ingresar_publicacion(datos)
-    if salida_hashtags != None and salida_publicacion !=None:
+    salida_hashtags = ingresar_hashtags(datos["hashtags"])
+    if salida_hashtags !=None and salida_publicacion!= None:
         publicacion_hashtag(salida_publicacion, salida_hashtags)
-
-        # se inician las operaciones para insertar en la base de datos 2
-        salida_hashtags = ingresar_hashtags_base2(datos["hashtags"])
-        salida_publicacion = ingresar_publicacion_base2(datos)
-        if salida_hashtags != None and salida_publicacion !=None:
-            publicacion_hashtag_base2(salida_publicacion, salida_hashtags)
-            return jsonify ({"mensaje":"Publicado en la base de datos"})
+        return jsonify({"mensaje": "Ok"})
     else:
-        return jsonify({"mensaje": "Ocurrio un error"})
+        return jsonify({"mensaje": "Error"})
 
 
 def ingresar_publicacion(publicacion):
-    cursor = mysql.get_db().cursor()
+    conexion1 = obtener_conexion1()
+    cursor = conexion1.cursor()
     id_publicacion = -1
     try:
-        slq = """INSERT INTO publicacion(nombre,comentario,fecha, upvotes,downvotes)
-        VALUES('{0}','{1}', str_to_date('{2}','%d/%m/%Y'), {3}, {4})""".format(publicacion["nombre"], publicacion["comentario"], publicacion["fecha"],
+        #revisar algo de las fechas
+        slq = """INSERT INTO Publicaciones(nombre,comentario,fecha, upvotes,downvotes)
+        VALUES('{0}','{1}', CONVERT(DATETIME,{2},103), {3}, {4})""".format(publicacion["nombre"], publicacion["comentario"], publicacion["fecha"],
         publicacion["upvotes"], publicacion["downvotes"])
         cursor.execute(slq)
-        mysql.get_db().commit()
-        #mando a obtener el id
-        sql = "SELECT LAST_INSERT_ID() from publicacion"
+        conexion1.commit()
+        #obtener el id reciente
+        sql = "SELECT @@IDENTITY"
         cursor.execute(sql)
         datos = cursor.fetchone()
-        cursor.close()
+        conexion1.close()
         return datos[0]
     except Exception as e:
         print(e)
         return None
+    conexion1.close()
         
 
 def ingresar_hashtags(hastags):
     hashtags_id = []
-    cursor = mysql.get_db().cursor()
+    conexion1 = obtener_conexion1()
+    cursor = conexion1.cursor()
     try:
         for hashtag in hastags:
-            sql = "SELECT id_hashtag from hashtag where descripcion = '{0}'".format(hashtag)
+            sql = "SELECT id from Hashtags where comentario = '{0}'".format(hashtag)
             cursor.execute(sql)
             datos = cursor.fetchone()
             if datos !=None:
                 hashtags_id.append(datos[0])
             else:
-                sql = """INSERT INTO hashtag(descripcion) VALUES('{0}')""".format(hashtag)
+                sql = """INSERT INTO Hashtags(comentario) VALUES('{0}')""".format(hashtag)
                 cursor.execute(sql)
-                mysql.get_db().commit()
- 
-                sql = "SELECT id_hashtag from hashtag where descripcion = '{0}'".format(hashtag)
+                conexion1.commit()
+                sql = "SELECT id from Hashtags where comentario = '{0}'".format(hashtag)
                 cursor.execute(sql)
                 datos = cursor.fetchone()
                 hashtags_id.append(datos[0])
@@ -98,88 +108,39 @@ def ingresar_hashtags(hastags):
         print(e)
         return None
     #return los id de los hashtags
-    cursor.close()
+    conexion1.close()
     return hashtags_id
 
 
+#revisar algo de aca que no inserta
 def publicacion_hashtag(id_publicacion, hashtags_id):
-    cursor = mysql.get_db().cursor()
+    conexion1 = obtener_conexion1()
+    cursor = conexion1.cursor()
     try:
         for id_hashtag in hashtags_id:
-            sql = "INSERT INTO publicacionhashtag(id_publicacion,id_hashtag) VALUES({0},{1})".format(id_publicacion,id_hashtag)
-            #print(sql)
+            sql = "INSERT INTO PublicacionesHashtags(PublicacionId,HashtagId) VALUES({0},{1})".format(id_publicacion,id_hashtag)
+            print(sql)
             cursor.execute(sql)
-            mysql.get_db().commit()
+            conexion1.commit()
     except Exception as e:
         print(e)
         return None
-    cursor.close()
+    conexion1.close()
 
+@app.route('/prueba',methods = ['GET'])
+def prueba():
+    conexion1 = obtener_conexion1()
+    cursor1 = conexion1.cursor()
+    cursor1.execute("SELECT * from PublicacionesHashtags")
+    print(cursor1.fetchall())
+    cursor1.execute("SELECT * from Publicaciones")
+    print(cursor1.fetchall())
+    cursor1.execute("SELECT * from Hashtags")
+    print(cursor1.fetchall())
+    cursor1.close()
+    return jsonify({"mensaje":"pong!"})
 
-"""  
-    Aqui se agregan las mismas operaciones pero con la base de datos numero 2
-"""
-def ingresar_publicacion_base2(publicacion):
-    cursor = mysql_1.get_db().cursor()
-    id_publicacion = -1
-    try:
-        slq = """INSERT INTO publicacion(nombre,comentario,fecha, upvotes,downvotes)
-        VALUES('{0}','{1}', str_to_date('{2}','%d/%m/%Y'), {3}, {4})""".format(publicacion["nombre"], publicacion["comentario"], publicacion["fecha"],
-        publicacion["upvotes"], publicacion["downvotes"])
-        cursor.execute(slq)
-        mysql_1.get_db().commit()
-        #mando a obtener el id
-        sql = "SELECT LAST_INSERT_ID() from publicacion"
-        cursor.execute(sql)
-        datos = cursor.fetchone()
-        cursor.close()
-        return datos[0]
-    except Exception as e:
-        print(e)
-        return None
-        
-
-def ingresar_hashtags_base2(hastags):
-    hashtags_id = []
-    cursor = mysql_1.get_db().cursor()
-    try:
-        for hashtag in hastags:
-            sql = "SELECT id_hashtag from hashtag where descripcion = '{0}'".format(hashtag)
-            cursor.execute(sql)
-            datos = cursor.fetchone()
-            if datos !=None:
-                hashtags_id.append(datos[0])
-            else:
-                sql = """INSERT INTO hashtag(descripcion) VALUES('{0}')""".format(hashtag)
-                cursor.execute(sql)
-                mysql_1.get_db().commit()
- 
-                sql = "SELECT id_hashtag from hashtag where descripcion = '{0}'".format(hashtag)
-                cursor.execute(sql)
-                datos = cursor.fetchone()
-                hashtags_id.append(datos[0])
-    except Exception as e:
-        print(e)
-        return None
-    #return los id de los hashtags
-    cursor.close()
-    return hashtags_id
-
-
-def publicacion_hashtag_base2(id_publicacion, hashtags_id):
-    cursor = mysql_1.get_db().cursor()
-    try:
-        for id_hashtag in hashtags_id:
-            sql = "INSERT INTO publicacionhashtag(id_publicacion,id_hashtag) VALUES({0},{1})".format(id_publicacion,id_hashtag)
-            #print(sql)
-            cursor.execute(sql)
-            mysql_1.get_db().commit()
-    except Exception as e:
-        print(e)
-        return None
-    cursor.close()
-
-
+#funcion de helpers
 def Convert(entrada):
     new_publicacion = {
         "nombre": entrada["nombre"],
