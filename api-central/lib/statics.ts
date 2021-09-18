1d66f2f1de213f78f485f9f77cb2f7b276f862fa
davideliaspc <davideliaspc@gmail.com>
2021-09-18 16:38:49 -0600

    Cambio menor en statics.

--- api-central/lib/statics.ts
+++ api-central/lib/statics.ts
@@ -1,14 +1,3 @@
 export const readCredentials     = ['api-read'   , 'WjJRGHJCxFy6TkowHkCQ'];
 export const writeCredentials    = ['api-write'  , 'qgqfc09MNMDv0kmTWVfL' ];
 export const deleteCredentials   = ['api-delete'  , 'KU0wkxYL0c6bziYUBkmk' ];
-
-// JWT Data
-export const secret = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYxNzU0OTQ2OCwiaWF0IjoxNjE3NTQ5NDY4fQ.FQSDsnxGe95Qe83q6Kqby9wWutiakN6rQ23zpCbPscw';
-export const saltRounds = 10;
-
-export const blobAzureStorageAccount = {
-    account: 'letmepass',
-    container: 'letmepass',
-    connectionString: 'DefaultEndpointsProtocol=https;AccountName=letmepass;AccountKey=IqfF1mrg0WMhdh4YlSAk8imXiEHsv/uKF/d/FtY7g2ftfLMilwM/GBLNKi6plf+6XEnAu0V6rTZt0paCPe7/xg==;EndpointSuffix=core.windows.net',
-    key: 'IqfF1mrg0WMhdh4YlSAk8imXiEHsv/uKF/d/FtY7g2ftfLMilwM/GBLNKi6plf+6XEnAu0V6rTZt0paCPe7/xg==',
-};
